cc7199f6d31bf03737d24afa51e317311c4aadc7
desibebek99-hash <desibebek99@gmail.com>
2026-06-24 00:09:39 +0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,35 @@
+import os
+import requests
+import feedparser
 
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+# Untuk sementara satu RSS dulu
+RSS_URL = os.environ["RSS_URL"]
+
+feed = feedparser.parse(RSS_URL)
+
+for item in feed.entries[:5]:
+
+    pesan = f"""
+🏠 Lead Properti Baru
+
+Judul:
+{item.title}
+
+Link:
+{item.link}
+"""
+
+    r = requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={
+            "chat_id": CHAT_ID,
+            "text": pesan
+        }
+    )
+
+    print(r.status_code)
+
+print("Selesai")
