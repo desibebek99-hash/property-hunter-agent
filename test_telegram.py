4db41f523a186f2289f610922e6762db3b7d167c
desibebek99-hash <desibebek99@gmail.com>
2026-06-23 23:28:25 +0700

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -1,1 +1,21 @@
+import requests
+import os
 
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = """
+🚀 Property Hunter Agent Aktif
+
+Tes notifikasi berhasil.
+"""
+
+requests.get(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    params={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Pesan terkirim")
